b6a45267e3c8eb7f0aea5d7bbe54ea3765ac3071
tai <tai.ntt>
2018-06-15 10:53:20 +0700

    push to heroku

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require("express");
+//Install express server
+const express = require('express');
+const path = require('path');
+
 const app = express();
-const path = require("path");
 
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
 
-app.get("/*",(req,res)=>{
-    res.sendFile(path.join(__dirname+ "/dist/index.html"));
-})
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 5000);
